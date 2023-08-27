9f26eff1c2fea9cec29f0fbcbf0bfd5b82d25cc5
rashidsarkar <rashidrock558@gmail.com>
2023-08-27 23:39:16 +0600

    sarch

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const loadPhone = async (phoneName) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${phoneName}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+
+  displayPhones(phones);
+};
+
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone_container");
+  // console.log(phone);
+  phones.forEach((phone) => {
+    console.log(phone);
+    //1 creat a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
+    phoneCard.innerHTML = `
+    <figure><img src="${phone.image}" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+    `;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+//handel btn
+const handleSarch = () => {
+  const sarcheFild = document.getElementById("sarche_fild");
+  const sarchText = sarcheFild.value;
+  console.log(sarchText);
+};
+// loadPhone();
